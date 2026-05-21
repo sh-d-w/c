ef448ed67990fa5d1af91d749a6459260f0b8e14
S𝔥𝔞⁽ᵃʰⁱ⁾d M𝔞𝔥𝔬𝔪𝔢ᵈ <31412774+sh-d-w@users.noreply.github.com>
2026-05-21 12:02:34 +0200

    Add files via upload

---
+++ choose/b.h
@@ -0,0 +1,52 @@
+/*                       __________________							  */
+/*                        S |-| - D - \/\/ 							  */
+/*                       ==================							  */
+/*																	  */
+/*   	   Original code developed during 42.fr curriculum. 		  */
+/* 			  Project renamed and refactored for clarity.			  */
+/*             Created: 2016-10-20 by S𝔥𝔞⁽ᵃʰⁱ⁾d M𝔞𝔥𝔬𝔪𝔢ᵈ				  */
+/*																	  */
+/*   Copyright [2026] [S𝔥𝔞⁽ᵃʰⁱ⁾d M𝔞𝔥𝔬𝔪𝔢ᵈ] under Apache 2.0 License	  */
+
+#ifndef B_H
+# define B_H
+# include <term.h>
+# include <termios.h>
+# include <termcap.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <sys/ioctl.h>
+
+typedef struct		s_e
+{
+	struct termios	term;
+	int				ac;
+	int				cursor;
+	char			**av;
+	int				*deleted;
+	int				*selected;
+	int				skip_u;
+	int				skip_d;
+	int				x;
+	int				y;
+	int				w;
+	int				h;
+	int				home;
+	int				end;
+	int				b;
+}					t_e;
+
+int					clear(void);
+int					tputs_putchar(int c);
+void				ft_catchsignal(int i);
+t_e					*ft_global(t_e *new, int i);
+int					tputs_putchar(int c);
+void				ft_resize(void);
+int					ft_winsizecheck(t_e *e);
+int					ft_print(t_e *e);
+int					skip(t_e *e);
+int					non_canonical(struct termios *term, char *status);
+#endif
---
+++ choose/e.c
@@ -0,0 +1,61 @@
+/*                       __________________							  */
+/*                        S |-| - D - \/\/ 							  */
+/*                       ==================							  */
+/*																	  */
+/*   	   Original code developed during 42.fr curriculum. 		  */
+/* 			  Project renamed and refactored for clarity.			  */
+/*             Created: 2016-10-20 by S𝔥𝔞⁽ᵃʰⁱ⁾d M𝔞𝔥𝔬𝔪𝔢ᵈ				  */
+/*																	  */
+/*   Copyright [2026] [S𝔥𝔞⁽ᵃʰⁱ⁾d M𝔞𝔥𝔬𝔪𝔢ᵈ] under Apache 2.0 License	  */
+
+#include "b.h"
+
+t_e			*ft_global(t_e *new, int i)
+{
+	static t_e	*e;
+
+	return (e + 0 * (!i && (e = new)));
+}
+
+int			tputs_putchar(int c)
+{
+	return (write(2, &c, 1));
+}
+
+int			clear(void)
+{
+	return (tputs(tgetstr("cl", 0), 1, tputs_putchar));
+}
+
+static int	ft_ft_putchar(int c)
+{
+	return (write(2, &c, 1));
+}
+
+int			non_canonical(struct termios *term, char *status)
+{
+	static int	n;
+
+	if (!strcmp(status, "start"))
+	{
+		if ((n += (n < 2)) && n == 1)
+		{
+			tcgetattr(0 * tgetent(0, getenv("TERM")), term);
+			tputs(tgetstr("ti", 0), 1, ft_ft_putchar);
+			tputs(tgetstr("ho", 0), 1, ft_ft_putchar);
+		}
+		term->c_lflag &= ~(ECHO | ICANON);
+		term->c_cc[VMIN] = 1;
+		term->c_cc[VTIME] = 0;
+		tcsetattr(0, 0, term);
+		tputs(tgetstr("vi", 0), 1, ft_ft_putchar);
+	}
+	else if (!strcmp(status, "end"))
+	{
+		term->c_lflag |= (ICANON | ECHO);
+		tputs(tgetstr("te", 0), 1, ft_ft_putchar);
+		tputs(tgetstr("ve", 0), 1, ft_ft_putchar);
+		tcsetattr(0, 0, term);
+	}
+	return (1);
+}
